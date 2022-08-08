dc8d6ebb1cefb8d578c8cc4586332785854770a9
jsPark <wotjddnfl@gmail.com>
2022-08-08 13:18:29 +0900

    api: auth api 함수 정의

---
+++ src/api/api.ts
@@ -0,0 +1,14 @@
+import { Axios } from "api/customAxios";
+import { authTypes } from "types/api";
+
+export const logIn = async (authInfo: authTypes) => {
+  const apiURL = "users/login";
+  const { data } = await Axios.post(apiURL, authInfo);
+  return data;
+};
+
+export const signUp = async (authInfo: authTypes) => {
+  const apiURL = "users/create";
+  const data = await Axios.post(apiURL, authInfo);
+  return data;
+};
---
+++ src/types/api.ts
@@ -0,0 +1,4 @@
+export interface authTypes {
+  email: string;
+  password: string;
+}
