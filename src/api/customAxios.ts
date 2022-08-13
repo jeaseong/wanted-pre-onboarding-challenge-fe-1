3b96798d1924b2063c37deb31b5835cf0423e4c0
jsPark <wotjddnfl@gmail.com>
2022-08-13 16:42:12 +0900

    refactor: add return error in axios interceptors

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -11,19 +11,22 @@ const Axios: AxiosInstance = axios.create({
   },
 });
 
-Axios.interceptors.request.use(async (config) => {
-  const accessToken = localStorage.getItem("userToken");
-  if (config && accessToken) {
-    config.headers = {
-      authorization: `${accessToken}`,
-    };
-  }
-  return config;
-});
+Axios.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem("userToken");
+    if (config && accessToken) {
+      config.headers = {
+        authorization: `${accessToken}`,
+      };
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 Axios.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject(error.response.data)
+  (error) => Promise.reject(error)
 );
 
 export { Axios };
