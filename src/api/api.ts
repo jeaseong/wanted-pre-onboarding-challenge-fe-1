93e2a40cde8f07311be35b068524df8a71645160
jsPark <wotjddnfl@gmail.com>
2022-08-15 23:45:23 +0900

    refactor: 불필요한 api return 값 정리

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import { Axios } from "api/customAxios";
+import { ParamsId } from "types/type";
 import { authTypes, todoType } from "types/api";
 
 export const logIn = async (authInfo: authTypes) => {
@@ -16,28 +17,30 @@ export const signUp = async (authInfo: authTypes) => {
 export const getTodos = async () => {
   const apiURL = "todos";
   const { data } = await Axios.get(apiURL);
-  return data;
+  return data.data;
 };
 
-export const getTodoById = async (id: string) => {
-  const apiURL = `todos/${id}`;
-  const { data } = await Axios.get(apiURL);
-  return data;
+export const getTodoById = async (id: ParamsId) => {
+  if (id) {
+    const apiURL = `todos/${id}`;
+    const { data } = await Axios.get(apiURL);
+    return data.data;
+  }
+  throw Error();
 };
 
 export const createTodo = async (todo: todoType) => {
   const apiURL = `todos`;
-  const { data } = await Axios.post(apiURL, todo);
-  return data;
+  await Axios.post(apiURL, todo);
 };
 
 export const updateTodo = async (id: string, todo: todoType) => {
   const apiURL = `todos/${id}`;
-  const { data } = await Axios.put(apiURL, todo);
-  return data;
+  await Axios.put(apiURL, todo);
+  console.log("ㅝ야?");
 };
 
-export const deleteTodo = async (id: string) => {
+export const deleteTodo = async (id: ParamsId) => {
   const apiURL = `todos/${id}`;
   await Axios.delete(apiURL);
 };
